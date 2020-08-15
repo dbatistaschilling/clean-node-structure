a33159ea6054e5bdd9fdca8bf3b2066955e76b44
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-08-15 21:38:40 +0200

    fix: misspell bugs

---
+++ src/main/routes/login-routes.ts
@@ -0,0 +1,7 @@
+// import { adaptRoute } from '../../infra/express/express-route-adapter'
+import { Router } from 'express'
+// import { makeLoginFactory } from './../factories/auth/LoginFactory'
+
+export default (router: Router): void => {
+  // router.post('/login', adaptRoute(makeLoginFactory()))
+}
